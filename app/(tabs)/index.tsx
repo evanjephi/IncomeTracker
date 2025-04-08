ce281aba6d0925fb9c53fcfc712eb8c89d395de3
Even <evanjephi@gmail.com>
2025-04-07 23:55:56 -0400

    status bar adjusted

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { ScrollView, TouchableOpacity, FlatList, View, Dimensions, Alert } from 'react-native';
+import { ScrollView, TouchableOpacity, FlatList, View, Dimensions, Alert, StatusBar } from 'react-native';
 import * as Print from 'expo-print';
 import * as Sharing from 'expo-sharing';
 import { IncomeContext } from '../IncomeContext';
@@ -199,160 +199,164 @@ const Index = () => {
   };
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      <ThemedView style={sharedStyles.container}>
-        <ThemedText style={[sharedStyles.header]}>Income Summary Report</ThemedText>
-        <ThemedText style={[sharedStyles.subHeader]}>Total Income</ThemedText>
-        <ThemedText style={[sharedStyles.total]}>Business Income: ${totalBusinessIncome.toFixed(2)}</ThemedText>
-        <ThemedText style={[sharedStyles.total]}>Salary Income: ${totalSalaryIncome.toFixed(2)}</ThemedText>
-        <ThemedText style={[sharedStyles.total, { fontWeight: 'bold' }]}>Overall Total Income: ${totalIncome.toFixed(2)}</ThemedText>
+    <>
+      {/* Configure the StatusBar to be transparent */}
+      <StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <ThemedView style={sharedStyles.container}>
+          <ThemedText style={[sharedStyles.header]}>Income Summary Report</ThemedText>
+          <ThemedText style={[sharedStyles.subHeader]}>Total Income</ThemedText>
+          <ThemedText style={[sharedStyles.total]}>Business Income: ${totalBusinessIncome.toFixed(2)}</ThemedText>
+          <ThemedText style={[sharedStyles.total]}>Salary Income: ${totalSalaryIncome.toFixed(2)}</ThemedText>
+          <ThemedText style={[sharedStyles.total, { fontWeight: 'bold' }]}>Overall Total Income: ${totalIncome.toFixed(2)}</ThemedText>
 
-        <TouchableOpacity onPress={() => toggleVisibility('business')} style={[sharedStyles.collapsibleHeader]}>
-          <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Business Income Allocations</ThemedText>
-        </TouchableOpacity>
-        {isBusinessVisible && (
-          <ThemedView style={sharedStyles.collapsibleContent}>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Tithe ({allocationPercentages.business.tithe}%):</ThemedText>
-              <ThemedText>${allocationTotals.business.tithe.toFixed(2)}</ThemedText>
+          <TouchableOpacity onPress={() => toggleVisibility('business')} style={[sharedStyles.collapsibleHeader]}>
+            <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Business Income Allocations</ThemedText>
+          </TouchableOpacity>
+          {isBusinessVisible && (
+            <ThemedView style={sharedStyles.collapsibleContent}>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Tithe ({allocationPercentages.business.tithe}%):</ThemedText>
+                <ThemedText>${allocationTotals.business.tithe.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Expense ({allocationPercentages.business.expenses}%):</ThemedText>
+                <ThemedText>${allocationTotals.business.expenses.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Education Fund ({allocationPercentages.business.educationFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.business.educationFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Business Fund ({allocationPercentages.business.businessFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.business.businessFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Fun Fund ({allocationPercentages.business.funFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.business.funFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Wealth Fund ({allocationPercentages.business.wealthFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.business.wealthFund.toFixed(2)}</ThemedText>
+              </ThemedView>
             </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Expense ({allocationPercentages.business.expenses}%):</ThemedText>
-              <ThemedText>${allocationTotals.business.expenses.toFixed(2)}</ThemedText>
+          )}
+          <TouchableOpacity onPress={() => toggleVisibility('salary')} style={[sharedStyles.collapsibleHeader]}>
+            <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Salary Income Allocations</ThemedText>
+          </TouchableOpacity>
+          {isSalaryVisible && (
+            <ThemedView style={sharedStyles.collapsibleContent}>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Tithe ({allocationPercentages.salary.tithes}%):</ThemedText>
+                <ThemedText>${allocationTotals.salary.tithes.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Expense ({allocationPercentages.salary.expenses}%):</ThemedText>
+                <ThemedText>${allocationTotals.salary.expenses.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Saving ({allocationPercentages.salary.saving}%):</ThemedText>
+                <ThemedText>${allocationTotals.salary.saving.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Education Fund ({allocationPercentages.salary.educationFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.salary.educationFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Debt ({allocationPercentages.salary.funFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.salary.funFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Wealth Fund ({allocationPercentages.salary.wealthFund}%):</ThemedText>
+                <ThemedText>${allocationTotals.salary.wealthFund.toFixed(2)}</ThemedText>
+              </ThemedView>
             </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Education Fund ({allocationPercentages.business.educationFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.business.educationFund.toFixed(2)}</ThemedText>
+          )}
+          <TouchableOpacity onPress={() => toggleVisibility('combined')} style={[sharedStyles.collapsibleHeader]}>
+            <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Combined Allocations</ThemedText>
+          </TouchableOpacity>
+          {isCombinedVisible && (
+            <ThemedView style={sharedStyles.collapsibleContent}>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Tithes:</ThemedText>
+                <ThemedText>${combinedAllocations.tithes.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Expenses:</ThemedText>
+                <ThemedText>${combinedAllocations.expenses.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Saving:</ThemedText>
+                <ThemedText>${combinedAllocations.saving.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Education Fund:</ThemedText>
+                <ThemedText>${combinedAllocations.educationFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Business Fund:</ThemedText>
+                <ThemedText>${combinedAllocations.businessFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Fun Fund & Debt:</ThemedText>
+                <ThemedText>${combinedAllocations.funFund.toFixed(2)}</ThemedText>
+              </ThemedView>
+              <ThemedView style={sharedStyles.allocationRow}>
+                <ThemedText>Wealth Fund:</ThemedText>
+                <ThemedText>${combinedAllocations.wealthFund.toFixed(2)}</ThemedText>
+              </ThemedView>
             </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Business Fund ({allocationPercentages.business.businessFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.business.businessFund.toFixed(2)}</ThemedText>
+          )}
+          <TouchableOpacity onPress={() => toggleVisibility('graph')} style={[sharedStyles.collapsibleHeader]}>
+            <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Graph</ThemedText>
+          </TouchableOpacity>
+          {isGraphVisible && (
+            <ThemedView style={sharedStyles.collapsibleContent}>
+              <AllocationGraph data={combinedAllocationData} />
             </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Fun Fund ({allocationPercentages.business.funFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.business.funFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Wealth Fund ({allocationPercentages.business.wealthFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.business.wealthFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-          </ThemedView>
-        )}
-        <TouchableOpacity onPress={() => toggleVisibility('salary')} style={[sharedStyles.collapsibleHeader]}>
-          <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Salary Income Allocations</ThemedText>
-        </TouchableOpacity>
-        {isSalaryVisible && (
-          <ThemedView style={sharedStyles.collapsibleContent}>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Tithe ({allocationPercentages.salary.tithes}%):</ThemedText>
-              <ThemedText>${allocationTotals.salary.tithes.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Expense ({allocationPercentages.salary.expenses}%):</ThemedText>
-              <ThemedText>${allocationTotals.salary.expenses.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Saving ({allocationPercentages.salary.saving}%):</ThemedText>
-              <ThemedText>${allocationTotals.salary.saving.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Education Fund ({allocationPercentages.salary.educationFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.salary.educationFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Debt ({allocationPercentages.salary.funFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.salary.funFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Wealth Fund ({allocationPercentages.salary.wealthFund}%):</ThemedText>
-              <ThemedText>${allocationTotals.salary.wealthFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-          </ThemedView>
-        )}
-        <TouchableOpacity onPress={() => toggleVisibility('combined')} style={[sharedStyles.collapsibleHeader]}>
-          <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Combined Allocations</ThemedText>
-        </TouchableOpacity>
-        {isCombinedVisible && (
-          <ThemedView style={sharedStyles.collapsibleContent}>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Tithes:</ThemedText>
-              <ThemedText>${combinedAllocations.tithes.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Expenses:</ThemedText>
-              <ThemedText>${combinedAllocations.expenses.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Saving:</ThemedText>
-              <ThemedText>${combinedAllocations.saving.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Education Fund:</ThemedText>
-              <ThemedText>${combinedAllocations.educationFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Business Fund:</ThemedText>
-              <ThemedText>${combinedAllocations.businessFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Fun Fund & Debt:</ThemedText>
-              <ThemedText>${combinedAllocations.funFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-            <ThemedView style={sharedStyles.allocationRow}>
-              <ThemedText>Wealth Fund:</ThemedText>
-              <ThemedText>${combinedAllocations.wealthFund.toFixed(2)}</ThemedText>
-            </ThemedView>
-          </ThemedView>
-        )}
-        <TouchableOpacity onPress={() => toggleVisibility('graph')} style={[sharedStyles.collapsibleHeader]}>
-          <ThemedText style={[sharedStyles.collapsibleHeaderText]}>Graph</ThemedText>
-        </TouchableOpacity>
-        {isGraphVisible && (
-          <ThemedView style={sharedStyles.collapsibleContent}>
-            <AllocationGraph data={combinedAllocationData} />
-          </ThemedView>
-        )}
+          )}
 
-        <ThemedView style={sharedStyles.allocationRow}>
-          <ThemedText>Total Over All Business Income:</ThemedText>
-          <ThemedText>${savedTotalBusinessIncome.toFixed(2)}</ThemedText>
+          <ThemedView style={sharedStyles.allocationRow}>
+            <ThemedText>Total Over All Business Income:</ThemedText>
+            <ThemedText>${savedTotalBusinessIncome.toFixed(2)}</ThemedText>
+          </ThemedView>
+          <ThemedView style={sharedStyles.allocationRow}>
+            <ThemedText>Total Over All Salary Income:</ThemedText>
+            <ThemedText>${savedTotalSalaryIncome.toFixed(2)}</ThemedText>
+          </ThemedView>
+
+          <TouchableOpacity
+            onPress={saveCurrentData}
+            style={[sharedStyles.button, { backgroundColor: '#006400' }]} // Dark green color
+          >
+            <ThemedText style={[sharedStyles.buttonText]}>Save All Data</ThemedText>
+          </TouchableOpacity>
+
+          <FlatList
+            data={pieChartData}
+            renderItem={renderPieChart}
+            keyExtractor={(item, index) => index.toString()}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          />
+
+          <TouchableOpacity
+            onPress={generatePDF}
+            style={[sharedStyles.button, { backgroundColor: '#0075be' }]} // Blue color for PDF button
+          >
+            <ThemedText style={[sharedStyles.buttonText]}>Generate Report</ThemedText>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={handleResetSavedData}
+            style={[sharedStyles.button, { backgroundColor: '#FF0000' }]} // Red color for reset button
+          >
+            <ThemedText style={[sharedStyles.buttonText]}>Reset Saved Data</ThemedText>
+          </TouchableOpacity>
         </ThemedView>
-        <ThemedView style={sharedStyles.allocationRow}>
-          <ThemedText>Total Over All Salary Income:</ThemedText>
-          <ThemedText>${savedTotalSalaryIncome.toFixed(2)}</ThemedText>
-        </ThemedView>
-
-        <TouchableOpacity
-          onPress={saveCurrentData}
-          style={[sharedStyles.button, { backgroundColor: '#006400' }]} // Dark green color
-        >
-          <ThemedText style={[sharedStyles.buttonText]}>Save All Data</ThemedText>
-        </TouchableOpacity>
-
-        <FlatList
-          data={pieChartData}
-          renderItem={renderPieChart}
-          keyExtractor={(item, index) => index.toString()}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-        />
-
-        <TouchableOpacity
-          onPress={generatePDF}
-          style={[sharedStyles.button, { backgroundColor: '#0075be' }]} // Blue color for PDF button
-        >
-          <ThemedText style={[sharedStyles.buttonText]}>Generate Report</ThemedText>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={handleResetSavedData}
-          style={[sharedStyles.button, { backgroundColor: '#FF0000' }]} // Red color for reset button
-        >
-          <ThemedText style={[sharedStyles.buttonText]}>Reset Saved Data</ThemedText>
-        </TouchableOpacity>
-      </ThemedView>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 };
 
